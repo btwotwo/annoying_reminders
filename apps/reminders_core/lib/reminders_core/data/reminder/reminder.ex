b866d2f055f91c47d9ee8dc3fa1421953ddba02a
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-01 21:57:16 +0200

    Move Ecto changeset to main reminder module

--- apps/reminders_core/lib/reminders_core/data/reminder/reminder.ex
+++ apps/reminders_core/lib/reminders_core/data/reminder/reminder.ex
@@ -1,44 +1,49 @@
 defmodule RemindersCore.Data.Reminder do
+  use Ecto.Schema
+
   @moduledoc """
-  It can be either standalone, or contain a list of `RemindersCore.Confirmation` that will trigger additional reminders after this one is completed.
-  An example reminder is "Did you eat?" that triggers daily at 12:00 with randomised 15-minute interval. One of confirmations is "Yes" that triggers another reminder "Did you eat pills?"
+  It can be either standalone, or contain a `Confirmation` that will trigger additional reminder after this one is completed.
   """
-  @enforce_keys [:id, :text, :firing_time, :firing_period, :firing_window]
-  defstruct [
-    :id,
-    :text,
-    :firing_time,
-    :firing_period,
-    :firing_window,
-    user_id: 0,
-    nagging_interval: 600
-  ]
+  schema "reminders" do
+    field(:text, :string)
+    field(:firing_time, :time)
+    field(:firing_period, Ecto.Enum, values: [:daily])
+    field(:firing_window, :integer)
+    field(:nagging_interval, :integer, default: 10000)
+    field(:ack_delay, :integer)
+    has_one(:state, RemindersCore.Data.Reminder.ReminderState)
+  end
 
   @typedoc """
   A reminder with the following fields:
   - `id`: Unique ID of reminder
   - `text`: Reminder text (e.g "Did you eat?")
   - `firing_time`: Time of day when reminder should fire.
-  - `firing_period`: How often the notification should be triggered.
+  - `firing_period`: How often the notification should be triggered (e.g `:daily`)
   - `firing_window`: Randomized time windows size. For example, if you set `firing_time` to 12:00 and window size to 15 mins, so it will randomly fire between 11:45 and 12:15.
-  - `nagging_interval`: Period indicating how often the notification should be resent. Defined in milliseconds, default value is 600.
+  - `nagging_interval`: Period indicating how often the notification should be resent. Defined in milliseconds, default value is 10000.
+  - `acknowledge_delay`: Delay before sending an acknowledgment notification. Optional.
   """
   @type t :: %__MODULE__{
           id: any(),
-          user_id: any(),
+          # user_id: any(),
           text: String.t(),
           firing_time: Time.t(),
-          firing_period: :daily,
-          firing_window: pos_integer(),
-          nagging_interval: pos_integer()
+          firing_period: :daily | nil,
+          firing_window: pos_integer() | nil,
+          nagging_interval: pos_integer(),
+          ack_delay: pos_integer() | nil
         }
 
-  def new(attrs)
-      when is_binary(attrs.text) and byte_size(attrs.text) > 0
-      when is_struct(attrs.firing_time, Time)
-      when attrs.firing_period in [:daily]
-      when is_number(attrs.firing_window)
-      when is_number(attrs.nagging_interval) and attrs.nagging_interval > 1000 do
-    struct!(__MODULE__, attrs)
+  def changeset(reminder, params \\ %{}) do
+    reminder
+    |> Ecto.Changeset.cast(params, [
+      :text,
+      :firing_time,
+      :firing_period,
+      :firing_window,
+      :nagging_interval
+    ])
+    |> Ecto.Changeset.validate_required([:text, :firing_time, :firing_period])
   end
 end
