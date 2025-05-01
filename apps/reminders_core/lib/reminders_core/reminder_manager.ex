a54f529c5857fd9b16ba7030e25c22dd28888738
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-01 21:58:08 +0200

    Add stub for ReminderManager module

---
+++ apps/reminders_core/lib/reminders_core/reminder_manager.ex
@@ -0,0 +1,13 @@
+defmodule RemindersCore.ReminderManager do
+  alias RemindersCore.Data.Reminder.Repo
+
+  def schedule_reminder(id) do
+     
+  end
+
+  def confirm_reminder(id) do
+  end
+
+  def acknowledge_reminder(id) do
+  end
+end
