23eaa0936f4ce38197ca7e627382ce6d623401fe
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-01 21:57:40 +0200

    Add stub for the users table

--- apps/reminders_core/priv/repo/migrations/20250429144941_create_reminders.exs
+++ apps/reminders_core/priv/repo/migrations/20250429144941_create_reminders.exs
@@ -2,12 +2,21 @@ defmodule RemindersCore.Data.Reminder.Repo.Migrations.CreateReminders do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      timestamps()
+    end
+    
     create table(:reminders) do
+      add :user_id,
+          references(:users, on_delete: :delete_all),
+          null: false
+
       add :text, :string, null: false
       add :firing_time, :time, null: false
       add :firing_period, :string, null: false
       add :firing_window, :integer, null: true
       add :nagging_interval, :integer, null: true
+      add :ack_delay, :integer, null: true
       timestamps()
     end
 
@@ -16,7 +25,7 @@ defmodule RemindersCore.Data.Reminder.Repo.Migrations.CreateReminders do
           references(:reminders, on_delete: :delete_all),
           primary_key: true,
           null: false
-      add :state, :integer, null: false
+      add :state, :string, null: false
       timestamps()
     end
   end
