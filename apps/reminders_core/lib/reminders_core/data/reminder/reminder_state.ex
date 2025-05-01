cfee716e371a543851c62b20a74becf4b42e2cb0
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-01 21:58:41 +0200

    Add docs to ReminderState, fix relationship module name

--- apps/reminders_core/lib/reminders_core/data/reminder/reminder_state.ex
+++ apps/reminders_core/lib/reminders_core/data/reminder/reminder_state.ex
@@ -1,8 +1,11 @@
 defmodule RemindersCore.Data.Reminder.ReminderState do
   use Ecto.Schema
-
+  @moduledoc """
+  Current state of the reminder. Updated by `RemindersCore.ReminderManager`. Every reminder has only one state record.
+  """
+  
   schema "reminder_states" do
     field(:state, Ecto.Enum, values: [:pending, :scheduled, :fired])
-    belongs_to(:reminder, RemindersCore.Data.Reminder.Reminderv2)
+    belongs_to(:reminder, RemindersCore.Data.Reminder)
   end
 end
