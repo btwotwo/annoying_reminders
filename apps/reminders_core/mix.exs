99b8d68ff7ae36991b98607bc7c174d0559a2780
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-03 23:00:53 +0200

    Add dependencies for tests

--- apps/reminders_core/mix.exs
+++ apps/reminders_core/mix.exs
@@ -11,7 +11,9 @@ defmodule RemindersCore.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      aliases: aliases()
     ]
   end
 
@@ -28,10 +30,18 @@ defmodule RemindersCore.MixProject do
     [
       {:ecto_sql, "~> 3.0"},
       {:postgrex, "~> 0.20.0"},
-      {:testcontainers, "~> 1.11"}
+      {:stream_data, "~> 1.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+  defp aliases do
+    [
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
